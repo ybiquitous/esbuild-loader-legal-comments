8b8fb9c3f70339df7941ae5220bc807c0f44fa23
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2022-07-15 09:25:56 +0900

    Add object-assign dependency

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import classnames from "classnames";
+import objectAssign from "object-assign";
 
 function longlong(longlongArg) {
   return classnames(longlongArg);
 }
 
 console.log(longlong());
+
+console.log(objectAssign({ foo: 0 }, { bar: 1 }));
